dae6058ef2f4a3d8bd8e4451362a3f5f6f78f5a4
yam-yam-yam-yam <77680103+yam-yam-yam-yam@users.noreply.github.com>
2022-07-19 20:48:08 +0900

    [feat] init_pinia

---
+++ src/store/user.js
@@ -0,0 +1,11 @@
+import { defineStore } from "pinia";
+
+export const storeUser = defineStore("user",{
+    state: () => {
+        return {}
+    },
+    actions: {
+    },
+    getters: {
+    }
+});
